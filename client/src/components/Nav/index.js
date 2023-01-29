4681cd799400be78f570181299a1222852de0067
Madisen <madisenvogel@gmail.com>
2023-01-29 15:22:45 -0700

    simplified seeds

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -43,7 +43,7 @@ function Nav() {
     <header className="flex-row px-1">
       <h1>
         <Link to="/">
-          <span role="img" aria-label="shopping bag"></span>
+          <span role="img"></span>
           TURNIP
         </Link>
       </h1>
