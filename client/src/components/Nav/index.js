5505b4d7ea9d757a64d270ec17a9f0973babb46c
Madisen <madisenvogel@gmail.com>
2023-01-29 15:04:58 -0700

    font changed:

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -43,8 +43,8 @@ function Nav() {
     <header className="flex-row px-1">
       <h1>
         <Link to="/">
-          <span role="img" aria-label="shopping bag">🛍️</span>
-          -Shop-Shop
+          <span role="img" aria-label="shopping bag"></span>
+          TURNIP
         </Link>
       </h1>
 
