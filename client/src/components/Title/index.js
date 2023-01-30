aaac639e54e6359c63b6f07d0d10a65fa6aa01bf
Madisen <madisenvogel@gmail.com>
2023-01-29 17:04:57 -0700

    video formatting

--- client/src/components/Title/index.js
+++ client/src/components/Title/index.js
@@ -3,18 +3,24 @@ import styled from "styled-components";
 import titleVideo from "./title.mp4"
 
 const VideoDiv = styled.div`
-    position: fixed;
-    z-index: -1;  
-    width: 100%;
+    top: 0;
+    left: 0;
+    width: auto;
+    height: auto; 
+    margin: 0;
+    padding: 0;
 `
 
 export default function Title() {
     return (
-        <VideoDiv>
-            <video autoPlay muted loop>
-                <source src={titleVideo} type="video/mp4"/>
-            </video>
-        </VideoDiv>
+        <div>
+            <VideoDiv>
+                <video autoPlay muted loop>
+                    <source src={titleVideo} type="video/mp4"/>
+                </video>
+            </VideoDiv>
+        </div>
+
 
 
     );
