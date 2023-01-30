ec10d5b7ad9b8802efd0fd86f28df4036328006f
Madisen Vogel <madisen@Admins-Mac-mini.btm.private>
2023-01-30 13:13:42 -0700

    title caption

--- client/src/components/Title/index.js
+++ client/src/components/Title/index.js
@@ -1,12 +1,34 @@
 import React from "react";
+import styled from "styled-components";
 import titleVideo from "./title.mp4";
+
+const ContentDiv = styled.div`
+    position: absolute;
+    bottom: 0;
+    background: rgba(0, 0, 0, 0.5);
+    color: #f1f1f1;
+    width: 100%;
+    padding: 20px;
+`;
+
+const Container = styled.div`
+    position: relative;
+`
 
 export default function Title() {
   return (
-    <div>
+    <Container>
         <video className="video" autoPlay muted loop>
           <source src={titleVideo} type="video/mp4" />
         </video>
-    </div>
+
+        <ContentDiv className="content">
+            <h1>About Us</h1>
+            <p>Lorem ipsum...</p>
+        </ContentDiv>
+    </Container>
+
+
+
   );
 }
