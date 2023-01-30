c99b7d7afd29fcecd8dbf9ccf19d7d87d9097d70
Madisen Vogel <madisen@Admins-Mac-mini.btm.private>
2023-01-30 13:26:08 -0700

    changing favicon

--- client/src/components/Title/index.js
+++ client/src/components/Title/index.js
@@ -5,10 +5,13 @@ import titleVideo from "./title.mp4";
 const ContentDiv = styled.div`
     position: absolute;
     bottom: 0;
+    right: 0;
     background: rgba(0, 0, 0, 0.5);
     color: #f1f1f1;
-    width: 100%;
-    padding: 20px;
+    width: auto;
+    padding: 50px;
+    margin-bottom: 20px;
+
 `;
 
 const Container = styled.div`
