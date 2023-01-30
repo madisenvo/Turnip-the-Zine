2022f8a07cabd43990244807ffdf4a52ad6bbe38
Madisen Vogel <madisen@Admins-Mac-mini.lan>
2023-01-30 12:13:32 -0700

    video fixed

--- client/src/components/Title/index.js
+++ client/src/components/Title/index.js
@@ -1,21 +1,12 @@
 import React from "react";
-import styled from "styled-components";
-import titleVideo from "./title.mp4"
-
-const VideoDiv = styled.div`
-    width: 100%vh;
-    height: 100%vw;
-
-`
+import titleVideo from "./title.mp4";
 
 export default function Title() {
-    return (
-        <div>
-            <VideoDiv>
-                <video autoPlay muted loop>
-                    <source src={titleVideo} type="video/mp4"/>
-                </video>
-            </VideoDiv>
-        </div>
-    );
+  return (
+    <div>
+        <video className="video" autoPlay muted loop>
+          <source src={titleVideo} type="video/mp4" />
+        </video>
+    </div>
+  );
 }
