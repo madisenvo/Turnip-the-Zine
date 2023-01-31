8cc3d3c58b59297af206b8ec644b7c4281e462d8
Madisen Vogel <madisen@Admins-Mac-mini.lan>
2023-01-31 14:40:58 -0700

    formatting comments

--- client/src/components/Thoughts/index.js
+++ client/src/components/Thoughts/index.js
@@ -1,25 +1,48 @@
 import React from "react";
-import 'bootstrap/dist/css/bootstrap.css';
-import './thoughts.css';
+import "bootstrap/dist/css/bootstrap.css";
+import "./thoughts.css";
 
 export default function Thoughts() {
-    return (
-<div className="container mt-5">
-        <div className="d-flex justify-content-center row">
-            <div className="col-md-6">
-                <div className="bg-white comment-section">
-                    <div className="d-flex flex-row user p-2">
-                        <div className="d-flex flex-column ml-2"><span className="name font-weight-bold">Chris Hemsworth</span><span>10:30 PM, May 25</span></div>
-                    </div>
-                    <div className="mt-2 p-2">
-                        <p className="comment-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                    </div>
-                    <div className="d-flex justify-content-between p-3 border-top"><span>Leave a comment</span>
-                        <div className="d-flex align-items-center border-left px-3 comments"><i className="fa fa-comment"></i><span className="ml-2"></span></div>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="container">
+      <div className="be-comment-block">
+        <div className="be-comment">
+          <div className="be-comment-content">
+            <span className="be-comment-name">
+              <h4>USER.FISTNAME</h4>
+            </span>
+            <span className="be-comment-time">
+              <i className="fa fa-clock-o"></i>
+              DATE/TIME
+            </span>
+
+            <p className="be-comment-text">
+              Pellentesque gravida tristique ultrices. Sed blandit varius
+              mauris, vel volutpat urna hendrerit id. Curabitur rutrum dolor
+              gravida turpis tristique efficitur.
+            </p>
+          </div>
         </div>
+        <div className="be-comment">
+          <div className="be-img-comment">
+            <a href="blog-detail-2.html"></a>
+          </div>
+        </div>
+        <div>
+          <form className="form-block">
+            <div className="col-xs-12">
+              <div className="form-group">
+                <textarea
+                  className="form-input"
+                  required=""
+                  placeholder="Your text"
+                ></textarea>
+              </div>
+            </div>
+            <a className="btn btn-primary pull-right">submit</a>
+          </form>
+        </div>
+      </div>
     </div>
-    );
+  );
 }
