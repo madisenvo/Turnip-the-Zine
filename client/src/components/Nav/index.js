3ffecb3de96a07bc432035619299b09bc11b699a
Sean Grant <104331199+seanmgrant@users.noreply.github.com>
2023-01-30 21:45:39 -0700

    photo in header deleted

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -2,6 +2,9 @@ import React from "react";
 import Auth from "../../utils/auth";
 import { Link } from "react-router-dom";
 
+
+
+
 function Nav() {
   function showNavigation() {
     if (Auth.loggedIn()) {
@@ -34,9 +37,12 @@ function Nav() {
 
   return (
     <header className="flex-row px-1">
+      
+      
       <h1>
         <Link to="/">
           {/* <span role="img"></span> */}
+          {/* <img src={photo} /> */}
           TURNIP
         </Link>
       </h1>
