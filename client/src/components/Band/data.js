2c880e17f9b369a4c8c754df14938fcad3641a3a
MarMarMo <108547874+MMockus15@users.noreply.github.com>
2023-01-31 14:28:46 -0700

    adding profiles with bootstrap

---
+++ client/src/components/Band/data.js
@@ -0,0 +1,35 @@
+import pinkRanger from "./bandImages/pinkranger.png";
+import chitChat from "./bandImages/chitchat.png"
+import amzy from "./bandImages/amzy.png"
+
+
+
+
+export const artists = [
+	{
+		imagePath: pinkRanger,
+		alt: "Pink Ranger profile picture",
+		title: "Pink Ranger",
+		titleTwo: "Musical Genius, All the ninja moves, Lover of pink, Basically famous.",
+		text: "Angsty Rock Artist",
+		footer: "PLAYING SATURDAY 2/18"
+	},
+	{
+		imagePath: chitChat,
+		alt: "ChitChat profile picture",
+		title: "ChitChat",
+		titleTwo: "Musical Genius, All the ninja moves, Lover of pink, Basically famous.",
+		text: "Angsty Rock Artist",
+		footer: "PLAYING SATURDAY 2/18"
+	},
+	{
+		imagePath: amzy,
+		alt: "Amzy profile picture",
+		title: "Amzy",
+		titleTwo: "Musical Genius, All the ninja moves, Lover of pink, Basically famous.",
+		text: "Angsty Rock Artist",
+		footer: "PLAYING SATURDAY 2/18"
+	},
+];
+
+export default artists;
