cf9099c3fdaa1a72a91bde09142c737e6d466146
Madisen Vogel <madisen@Admins-Mac-mini.lan>
2023-01-31 15:09:38 -0700

    formatting

--- client/src/components/Thoughts/index.js
+++ client/src/components/Thoughts/index.js
@@ -4,18 +4,17 @@ import "./thoughts.css";
 export default function Thoughts() {
   return (
     <div className="container">
-      <div className="be-comment-block">
-        <div className="be-comment">
-          <div className="be-comment-content">
-            <span className="be-comment-name">
+      <div className="commentDiv">
+        <div>
+          <div>
+            <span>
               <h4>USER.FISTNAME</h4>
             </span>
-            <span className="be-comment-time">
-              <i className="fa fa-clock-o"></i>
+            <span>
               DATE/TIME
             </span>
 
-            <p className="be-comment-text">
+            <p className="commentText">
               Pellentesque gravida tristique ultrices. Sed blandit varius
               mauris, vel volutpat urna hendrerit id. Curabitur rutrum dolor
               gravida turpis tristique efficitur.
@@ -23,7 +22,7 @@ export default function Thoughts() {
           </div>
         </div>
         <div className="be-comment">
-          <div className="be-img-comment">
+          <div>
             <a href="blog-detail-2.html"></a>
           </div>
         </div>
