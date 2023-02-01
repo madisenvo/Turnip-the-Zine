af46f11b0a47f9c11668893052c20dea0333f604
Madisen <madisenvogel@gmail.com>
2023-01-31 20:12:59 -0700

    success page formatted

--- client/src/pages/Success.js
+++ client/src/pages/Success.js
@@ -31,11 +31,9 @@ function Success() {
 
   return (
     <div className="success">
-      <Jumbotron>
         <h1>Success!</h1>
         <h2>Thank you for your purchase!</h2>
         <h2>You will now be redirected to the home page.</h2>
-      </Jumbotron>
     </div>
   );
 }
